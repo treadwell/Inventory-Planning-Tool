9775c5f6da8f5098dc45bc854a646bf722f5116b
treadwell <brooks.ken@gmail.com>
2014-06-29 14:43:41 -0400

    Impl Demand, Returns Plan classes and scenarios

--- scripts/IPT_classes.py
+++ scripts/IPT_classes.py
@@ -14,43 +14,24 @@ class Title(object):
         self.title_name = title_name
         self.cost = cost
 
-    def output(self):
-        ''' Print variables and values'''  # there has to be a better way to do this!
-        print "title_name :", self.title_name
-        print "cost: ", self.cost
+    def __repr__(self):
+        return str(self.__dict__)
 
 
 class Demand_Plan(object):
-    def __init__(self, title, title_demand_plan, title_returns_plan):
-        self.title = title
-        self.title_demand_plan = title_demand_plan
-        self.title_returns_plan = title_returns_plan
-        forecast = self.title_demand_plan.calc_forecast()
-        sd_forecast = self.title_demand_plan.calc_sd_forecast()
-        returns_forecast = self.title_returns_plan.calc_returns_forecast()
-
-    def output(self):
-        ''' Print variables and values'''  # there has to be a better way to do this!
-        print "title_name :", self.title_name
-        print "cost: ", self.cost
-        print "forecast", self.forecast
-
-
-class Title_Demand_Plan(Demand_Plan):
     def __init__(self, title, starting_monthly_demand, number_months, trendPerMonth, seasonCoeffs, initial_cv, per_period_cv):
-        self.title = title
+        self.title_name = title.title_name
         self.starting_monthly_demand = starting_monthly_demand
         self.number_months = number_months
         self.trendPerMonth = trendPerMonth
         self.seasonCoeffs = seasonCoeffs
         self.initial_cv = initial_cv
         self.per_period_cv = per_period_cv
+        self.months, self.forecast = self.calc_forecast()
+        self.sd_forecast = self.calc_sd_forecast()
         
-    def output(self):
-        ''' Print variables and values'''  # there has to be a better way to do this!
-        print "title: ", self.title
-        print "starting_monthly_demand: ", self.starting_monthly_demand
-        print "number_months:", self.number_months
+    def __repr__(self):
+        return str(self.__dict__)
 
     def calc_forecast(self):
         '''Returns two lists holding a sequential month number and a second with
@@ -79,25 +60,43 @@ class Title_Demand_Plan(Demand_Plan):
 
         return sd_forecast
 
+class Aggressive_Demand_Plan(Demand_Plan):
+     # self.starting_monthly_demand *= 2 # would return an error, there's no
+     # starting_monthly_demand defined
+ 
+    def __init__(self, *args):
+        super(Aggressive_Demand_Plan, self).__init__(*args)
+        #Demand_Plan.__init__(self, *args) # also an option
+        #super().__init__(self, *args) ## Python 3
+        self.starting_monthly_demand *= 2
+        self.months, self.forecast = self.calc_forecast()
+        self.sd_forecast = self.calc_sd_forecast()
 
+class Conservative_Demand_Plan(Demand_Plan):
+    def __init__(self, *args):
+        super(Conservative_Demand_Plan, self).__init__(*args)
+        self.starting_monthly_demand /= 2.0
+        self.months, self.forecast = self.calc_forecast()
+        self.sd_forecast = self.calc_sd_forecast()
 
-class Title_Returns_Plan(Demand_Plan):
-    def __init__(self, title, returns_rate, lag):
-        self.title = title
+class Returns_Plan(object):
+    def __init__(self, title, demand_plan, returns_rate, lag):
+        self.title_name = title.title_name
         self.returns_rate = returns_rate
         self.lag = lag
+        self.months = demand_plan.months
+        self.forecast = demand_plan.forecast
+        self.returns = self.calc_returns_forecast()
     
-    def output(self):
-        ''' Print variables and values'''  # there has to be a better way to do this!
-        print "title: ", self.title
-        print "returns_rate: ", self.returns_rate
-        print "lag:", self.lag
+    def __repr__(self):
+        return str(self.__dict__)
 
     def calc_returns_forecast(self):
         '''Calculate a returns forecast based by applying a percentage and lag to demand forecast'''
 
         returns = [0]*len(self.forecast)
-        for i,x in enumerate(self.forecast):
+
+        for i, x in enumerate(self.forecast):
             if i < lag:
                 returns[i] = 0
             else:
@@ -105,26 +104,14 @@ class Title_Returns_Plan(Demand_Plan):
         return returns
 
 
-class Supply_Plan(object):
-    def __init__(title, purchasing_plan, print_plan, ss_plan):
-        self.title = title
-        self.purchasing_plan = purchasing_plan
-        self.print_plan = print_plan
-        self.ss_plan = ss_plan
-
-    def output(self):
-        print self.summary_stats
-        print self.dataframe
-        print self.plots
-
-
-
-
-class Purchasing(object):
+class Purchase_Plan(object):
     """Base class for purchasing strategies"""
 
     def __init__(self, title):
-        self.title = title
+        self.title_name = title.title_name
+
+    def __repr__(self):
+        return str(self.__dict__)
 
     def subclassable_fn(self):
         raise NotImplementedError # fn that's going to be implemented in a subclass
@@ -134,23 +121,65 @@ class Purchasing(object):
         pass
 
 
-    def output(self):
-        return self.month_supply()
-
-
-class month_supply(Purchasing):
+class month_supply(Purchase_Plan):
     def subclassable_fn(self):
         return 42
 
-class EOQ(Purchasing):
+class EOQ(Purchase_Plan):
     """Economic Order Quantity"""
 
     def subclassable_fn(self):
         import random
         return random.randint(1, 500)
 
+class Print_Plan(object):
+    """Base class for print strategies"""
+
+    def __init__(self, title):
+        self.title_name = title.title_name
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+class SS_Plan(object):
+    """Base class for safety stock strategies"""
+    
+    def __init__(self, title):
+        self.title_name = title.title_name
+
+    def __repr__(self):
+        return str(self.__dict__)
 
 
+def scenario(title, Demand_Plan, Returns_Plan, Print_Plan, Purchase_Plan, SS_Plan):
+    # title should be an actual object
+    # the other inputs should be classes
+
+    # capture user input here (in the future), for now just enter the parameters by hand
+    # Demand Plan parameters
+    starting_monthly_demand = 1000
+    number_months = 36
+    trendPerMonth = -0.05
+    seasonCoeffs = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
+    initial_cv = 0.15
+    per_period_cv = 0.015
+
+    # Returns Plan parameters
+    returns_rate = 0.2
+    lag = 3
+
+
+    d = Demand_Plan(title, starting_monthly_demand, number_months, trendPerMonth, seasonCoeffs, initial_cv, per_period_cv)
+    r = Returns_Plan(title, d, returns_rate, lag)
+    pr = Print_Plan(title)
+    pu = Purchase_Plan(title)
+    ss = SS_Plan(title)
+
+    # put complete data frame and summary stats in here, but for now...
+
+    stats = map(sum, [d.forecast, r.returns])
+
+    return stats
 
 # ### in scenario.py:
 # for pp in plans.purchasing_plans:
@@ -176,12 +205,11 @@ if __name__ == '__main__':
     cost = cost = {'perOrder': 80.0, 'WACC': 0.12, 'POD_vmc': 5.0, 'fmc': 1000, 
         'vmc': 2.0, 'lost_margin': 10.00, 'allow_POD':True}
 
-    print "\n-------------test class Title-------------"
+    print "\n------------- test class Title -------------"
     xyz = Title("xyz", cost)
-    print "xyz:", xyz
-    xyz.output()
+    print "object xyz:", xyz
 
-    print "\n-------------test class Title_Demand_Plan-------------"
+    print "\n------------- test class Demand_Plan -------------"
     starting_monthly_demand = 1000
     number_months = 36
     trendPerMonth = -0.05
@@ -190,25 +218,56 @@ if __name__ == '__main__':
     initial_cv = 0.15
     per_period_cv = 0.015
 
-    title_demand_plan_1 = Title_Demand_Plan("xyz", starting_monthly_demand, number_months, trendPerMonth, seasonCoeffs, initial_cv, per_period_cv)
-    print "title_demand_plan_1:", title_demand_plan_1
-    title_demand_plan_1.output()
-    print title_demand_plan_1.calc_forecast()  # shouldn't this pull arguments from the object?
-    print title_demand_plan_1.calc_sd_forecast()
+    demand_plan_1 = Demand_Plan(xyz, starting_monthly_demand, number_months, trendPerMonth, seasonCoeffs, initial_cv, per_period_cv)
+    print "object demand_plan_1:", demand_plan_1
+    
+    print "months:", demand_plan_1.months
+    print "forecast:", demand_plan_1.forecast
+    print "sd_forecast:", demand_plan_1.sd_forecast
 
-    print  "\n-------------test class Title_Returns_Plan-------------"
+    print "\n------------- test class Aggressive_Demand_Plan -------------"
+    demand_plan_2 = Aggressive_Demand_Plan(xyz, starting_monthly_demand, number_months, trendPerMonth, seasonCoeffs, initial_cv, per_period_cv)
+    
+    print "months:", demand_plan_2.months
+    print "forecast:", demand_plan_2.forecast
+    print "sd_forecast:", demand_plan_2.sd_forecast
 
+    print "\n------------- test class Conservative_Demand_Plan -------------"
+    demand_plan_3 = Conservative_Demand_Plan(xyz, starting_monthly_demand, number_months, trendPerMonth, seasonCoeffs, initial_cv, per_period_cv)
+    
+    print "months:", demand_plan_3.months
+    print "forecast:", demand_plan_3.forecast
+    print "sd_forecast:", demand_plan_3.sd_forecast
+
+
+    print  "\n------------- test class Returns_Plan -------------"
     returns_rate = 0.2
     lag = 3
-    title_returns_plan_1 = Title_Returns_Plan("xyz", returns_rate, lag)
-    title_returns_plan_1.output()
-    # title_returns_plan_1.calc_returns_forecast()
+    returns_plan_1 = Returns_Plan(xyz, demand_plan_1, returns_rate, lag)
+    print "object returns_plan_1:", returns_plan_1
+    print "returns:", returns_plan_1.returns
 
-    print "\n-------------test class Demand_Plan-------------"
+    print  "\n------------- test class Print_Plan -------------"
 
-    demand_plan_1 = Demand_Plan(xyz, title_demand_plan_1, title_returns_plan_1)
+    print_plan_1 = Print_Plan(xyz)
+    print "object print_plan_1:", print_plan_1
 
 
+    print  "\n------------- test class Purchase_Plan -------------"
 
+    purchase_plan_1 = Purchase_Plan(xyz)
+    print "object purchase_plan_1:", purchase_plan_1
+
+
+    print  "\n------------- test class SS_Plan -------------"
+
+    ss_plan_1 = SS_Plan(xyz)
+    print "object ss_plan_1:", ss_plan_1
+
+    print  "\n------------- test scenario function -------------"
+
+    print scenario(xyz, Demand_Plan, Returns_Plan, Print_Plan, Purchase_Plan, SS_Plan)
+    print scenario(xyz, Aggressive_Demand_Plan, Returns_Plan, Print_Plan, Purchase_Plan, SS_Plan)
+    print scenario(xyz, Conservative_Demand_Plan, Returns_Plan, Print_Plan, Purchase_Plan, SS_Plan)
 
 
